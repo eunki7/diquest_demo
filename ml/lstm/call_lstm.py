2ddef5577f8a64f2d7269e498c7483b1baff0b3b
nation909 <threenation9@naver.com>
2019-04-01 21:04:56 +0900

    LSTM commit

---
+++ ml/lstm/call_lstm.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+
+from IPython import display
+from keras.preprocessing import sequence
+from konlpy.tag import Twitter
+from numpy.ma import argmax
+from tensorflow.python.keras.models import load_model
+import numpy as np
+import pandas as pd
+
+# LSTM 모델 사용하기 py
+
+# 상담콜 전처리 데이터 csv 로드
+data = pd.read_csv('../dataset/call_preprocessing.csv', encoding='euc-kr', delimiter=',')
+input_callText = data['STT_CONT'][2]
+result_true = data['CALL_L_CLASS_CD'][2]
+# print("원본 상담콜: ", input_callText)
+
+twitter = Twitter()
+stopwords = ['네네', '디큐']  # 금지어
+nounLength = 2  # okt 명사추출 글자수 이상
+callWordNum = 100
+
+# 상담콜 명사 index 데이터 csv 로드
+call_result = pd.read_csv('../dataset/call_result.csv', encoding='euc-kr', delimiter=',')
+nounsIndex = call_result['nounsAllIndex']
+
+# 명사 사전 index 형변환
+nounsIndexDict = {}
+for i in nounsIndex:
+    temp = i.strip("{}").replace("'", "").split(": ")
+    nounsIndexDict[temp[0]] = int(temp[1])
+print("명사 사전 index: ", nounsIndexDict)
+
+# 상담콜 명사 추출
+contNouns = []
+tempList = twitter.nouns(input_callText)
+contNouns.append([noun for noun in tempList if noun not in stopwords])
+print("상담콜 명사 추출: ", contNouns)
+
+# 상담콜 명사 index 추출
+input_callIndex = []
+for i in contNouns[0]:
+    for keyword in nounsIndexDict.keys():
+        if i == keyword:
+            input_callIndex.append(nounsIndexDict.get(i))
+print("상담콜 명사 index 추출: ", type(input_callIndex), input_callIndex)
+
+# 모델 로드
+model = load_model('./model/call_lstm_model.hdf5')
+
+# input 데이터 상담유형 예측
+x_test = sequence.pad_sequences([input_callIndex], maxlen=callWordNum)
+print("input_callIndex: ", x_test)
+result_predict = model.predict(x_test)
+# print("모델 예측율: ", result_predict)
+
+calltype = argmax(result_predict)
+print("정답유형: %d, 예측유형: %d, 예측율: %.2f%%" % (result_true, calltype, (result_predict[0][calltype] * 100)))
